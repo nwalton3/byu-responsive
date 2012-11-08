46d8cfb4d7ad4227a0132b2a120a8249098349a6
Nathan Walton <nwalton@byu.edu>
2012-11-08 16:51:54 -0700

    Initial commit

---
+++ home/config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "sites/default/files/css"
+sass_dir = "sites/default/files/css-sass"
+images_dir = "sites/all/themes/homepage/img"
+javascripts_dir = "sites/default/files/js"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css-sass scss && rm -rf sass && mv scss sass
